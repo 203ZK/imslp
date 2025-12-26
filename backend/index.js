e2583570d1f85523dff1e360c1f2fdab6b336b3d
203ZK <houjinwork@gmail.com>
2025-12-26 17:54:13 +0800

    Remove extra files

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,14 @@
 import express from "express";
 import cors from 'cors';
-// import { getWorksByQuery } from "./controllers/works.js";
-import { getScoreByLink, getScoresByWorkId } from "./controllers/scores.js";
+import { getScoreByLink } from "./scores.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
-
-// Parse incoming requests with URL-encoded payloads (form data)
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/:query", getWorksByQuery);
-// app.get("/work/:workId", getScoresByWorkId);
 app.post("/score", getScoreByLink);
 
 app.listen(PORT, () => {
