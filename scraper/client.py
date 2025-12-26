f82593dc07038eaab6c4a2304f8109d8a940625e
203ZK <houjinwork@gmail.com>
2025-12-26 17:59:51 +0800

    Fix port

--- scraper/client.py
+++ scraper/client.py
@@ -56,19 +56,38 @@ def getRowRange(page_num: int, page_size: int):
 
 async def main():
     load_dotenv()
-    url: str = os.getenv("SUPABASE_URL")
-    service_role_key: str = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+    url: str = os.getenv("SUPABASE_URL2")
+    service_role_key: str = os.getenv("SUPABASE_SERVICE_ROLE_KEY2")
     supabase: AsyncClient = await acreate_client(url, service_role_key)
 
-    MAX_PAGE_COUNT = 2531  # First one 147-500, 2nd 501-1000, 3rd 1001-1500, 4th 1501-2000, 5th 2001-2500, 6th 2501-2531
-    page_num = 1 # Skipped last few rows of page 1898
-    page_size = 100
+    try:
+        # id, work_title, composer, perm_link = work.values()
+        id = 228059
+        composer = "Beethoven, Ludwig van"
+        work_title = "Symphony No.9, Op.125"
+        all_scores = processWork("https://imslp.org/wiki/Symphony_No.9%2C_Op.125_(Beethoven%2C_Ludwig_van)", id)
+
+        if len(all_scores) == 0:
+            raise Exception(f"work contains no scores: {work_title}")
+        
+        await uploadScores(supabase, all_scores)
+        logger.info(f"Successfully processed work: ID {id}, {work_title} ({composer})")
+
+    except Exception as e:
+        logger.error(f"Error processing work: {e}")
+
+        with open("errors.txt", "a") as f:
+            f.write(f"client.py - Error processing work {id}: {e}\n")
+
+    # MAX_PAGE_COUNT = 2531  # First one 147-500, 2nd 501-1000, 3rd 1001-1500, 4th 1501-2000, 5th 2001-2500, 6th 2501-2531
+    # page_num = 1 # Skipped last few rows of page 1898
+    # page_size = 100
     
-    while page_num <= MAX_PAGE_COUNT: # current last page is page 2531
-        await processPage(supabase, page_num, page_size)
-        logger.info(f"Finished processing page {page_num}")
-        print(HORIZONTAL_BAR)
-        page_num += 1
+    # while page_num <= MAX_PAGE_COUNT: # current last page is page 2531
+    #     await processPage(supabase, page_num, page_size)
+    #     logger.info(f"Finished processing page {page_num}")
+    #     print(HORIZONTAL_BAR)
+    #     page_num += 1
     
 if __name__ == "__main__":
     run(main())
